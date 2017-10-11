b2a57f06de8d11e5ef38d48f4e80124ed6588450
DongGyu Lee <brainbackdoor@gmail.com>
2017-10-11 14:47:25 +0900

    [codesquad 테스트][볼링게임] 여러명 게임 기능 확인

--- src/test/java/bowling/GameTest.java
+++ src/test/java/bowling/GameTest.java
@@ -10,6 +10,25 @@ import org.junit.Test;
 public class GameTest {
 	Game game = new Game();
 	List<Player> players = new ArrayList<>();
+
+	@Test
+	public void 여러명_게임_테스트() {
+		game.init();
+		players = game.getPlayers();
+		
+		for (Player player : players) {
+			game.playGame(player);
+			for (int i = 0; i < 10; i++) {
+				System.out.println(player.getScore().getframeScore(i*2)
+						+" + "
+						+ player.getScore().getframeScore(i*2+1)
+						+" = "
+						+ (player.getScore().getframeScore(i*2)+player.getScore().getframeScore(i*2+1)));
+			}		
+			System.out.println("");
+		}
+
+	}
 	
 	@Test
 	public void 한명_게임_테스트() {
