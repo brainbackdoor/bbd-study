fa5f48222f52a85d7083ec96df909f06612ca582
seulgiwendy <wheejuni@gmail.com>
2017-12-20 15:15:28 +0900

    [git]reorganized project structure

---
+++ motherchoice/src/main/java/com/educhoice/motherchoice/MotherchoiceApplication.java
@@ -0,0 +1,12 @@
+package com.educhoice.motherchoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotherchoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotherchoiceApplication.class, args);
+	}
+}
