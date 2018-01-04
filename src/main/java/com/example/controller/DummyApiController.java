213882f2066834e1cce1bedd19a546fe8125a5e8
edusqaud <edusquad@gmail.com>
2018-01-05 02:17:08 +0900

    [Dummy] Add CrossOrigin Annotation

--- src/main/java/com/example/controller/DummyApiController.java
+++ src/main/java/com/example/controller/DummyApiController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,17 +23,18 @@ public class DummyApiController {
 
 	@Autowired
 	SearchAddressRepository searchAddressRepository;
-	
+	@CrossOrigin
 	 @GetMapping("/dummies")
 	 public List<SearchAddress> returnDummyAll() {
 	 return searchAddressRepository.findAll();
 	 }
 
+	@CrossOrigin
 	@GetMapping("/dummy/{key}")
 	public SearchAddress returnDummy(@PathVariable("key") long key) {
 		return searchAddressRepository.findByKey(key);
 	}
-
+	@CrossOrigin
 	@GetMapping("/dummy/contain/{address}")
 	public List<SearchAddress> findByAddressContaining(@PathVariable("address") String address) {
 		return searchAddressRepository.findByValueContaining(address);
