b9ab939000f0ef0ffda9a49b242ba627b868d283
seulgiwendy <wheejuni@gmail.com>
2018-01-15 19:50:31 +0900

    [model] AcademyRepository 수정

--- motherchoice/src/main/java/com/educhoice/motherchoice/models/persistent/repositories/AcademyRepository.java
+++ motherchoice/src/main/java/com/educhoice/motherchoice/models/persistent/repositories/AcademyRepository.java
@@ -4,6 +4,8 @@ import com.educhoice.motherchoice.models.persistent.HashTag;
 import com.educhoice.motherchoice.models.persistent.Academy;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +14,5 @@ public interface AcademyRepository extends CrudRepository<Academy, Long> {
     List<Academy> findByAcademyNameContaining(String academyName);
     Optional<Academy> findByAcademyId(long id);
     List<Academy> findByTagsContaining(HashTag tag);
+    List<Academy> findByCourses_DateTime_StartTimeBetweenAndEndTimeLessThan(LocalDate startTime, LocalTime endTime, LocalTime timeLimit);
 }
