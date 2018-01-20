0157f15e1bb7bb15875930df178fb206c7ed700f
seulgiwendy <wheejuni@gmail.com>
2018-01-20 16:12:11 +0900

    [security]JWT 적용을 위한 설정 수정

---
+++ motherchoice/src/main/java/com/educhoice/motherchoice/configuration/security/ResourceServerConfig.java
@@ -0,0 +1,59 @@
+package com.educhoice.motherchoice.configuration.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import sun.reflect.annotation.ExceptionProxy;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Qualifier("jwkTokenServices")
+    @Autowired
+    private ResourceServerTokenServices tokenService;
+
+    @Value("testjwtresourceid")
+    private String resourceIds;
+
+    @Value("realm")
+    private String securityRealm;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId(resourceIds).tokenServices(tokenService);
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .requestMatchers()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").authenticated();
+
+        httpSecurity
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .httpBasic()
+                .realmName(securityRealm)
+                .and()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .csrf()
+                .disable();
+    }
+
+
+}
--- motherchoice/src/main/java/com/educhoice/motherchoice/configuration/security/SecurityConfig.java
+++ motherchoice/src/main/java/com/educhoice/motherchoice/configuration/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.educhoice.motherchoice.configuration.security;
 
+import com.educhoice.motherchoice.configuration.security.service.AccountDetailsService;
 import com.educhoice.motherchoice.configuration.security.service.OAuthClientResource;
 import com.educhoice.motherchoice.configuration.security.service.OAuthUserTokenService;
 import com.educhoice.motherchoice.configuration.security.service.SocialSigninProviders;
@@ -11,7 +12,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.Ordered;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,38 +27,37 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CompositeFilter;
 
+import javax.management.MXBean;
 import javax.servlet.Filter;
 import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-@EnableOAuth2Client
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String signature = "9A59mXiu9oahIwkZB1Q4wSAQONKEbLMmzQ0R89rQAmh4yqutfTqyOeZ6tyhJoN9";
 
     @Qualifier("oauth2ClientContext")
     @Autowired
     OAuth2ClientContext clientContext;
 
+    @Autowired
+    private AccountDetailsService accountDetailsService;
+
 
     @Override
-    public void configure(HttpSecurity security) throws Exception {
-        security
-                .antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .antMatchers("/h2-console/**").permitAll()
-                .and()
-                .addFilterBefore(generateFilterSet(), BasicAuthenticationFilter.class);
-
-        security
-                .csrf().disable();
-
-        security
-                .headers().frameOptions().disable();
-
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(this.accountDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
@@ -62,44 +66,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        registrationBean.setFilter(filter);
-        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return registrationBean;
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
     }
 
     @Bean
-    @ConfigurationProperties("naver")
-    public OAuthClientResource naverResources() {
-        return new OAuthClientResource();
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+
+        return defaultTokenServices;
     }
 
     @Bean
-    @ConfigurationProperties("kakao")
-    public OAuthClientResource kakaoResources() {
-        return new OAuthClientResource();
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
-    private Filter generateFilterSet() {
-        CompositeFilter filter = new CompositeFilter();
-
-        List<Filter> filters = Lists.newArrayList();
-        filters.add(ssoFilter(naverResources(), "/login/naver", SocialSigninProviders.NAVER));
-        filters.add(ssoFilter(kakaoResources(), "/login/kakao", SocialSigninProviders.KAKAO));
-        filter.setFilters(filters);
-
-        return filter;
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signature);
+        return converter;
     }
 
-    private Filter ssoFilter(OAuthClientResource resource, String path, SocialSigninProviders providers) {
-        OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
-        OAuth2RestTemplate template = new OAuth2RestTemplate(resource.getClientDetails(), clientContext);
-
-        filter.setRestTemplate(template);
-        filter.setTokenServices(new OAuthUserTokenService(resource));
-        filter.setAuthenticationSuccessHandler(new IntegratedOAuthSigninSuccessHandler("/", providers));
-
-        return filter;
-    }
 }
