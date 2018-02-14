0725a55d6def3aa9f48d88f73f40a8f5a843e387
brainbackdoor <brainbackdoor@gmail.com>
2018-02-15 02:32:51 +0900

    Node JS Study - Step 01 - Callback Function 개념

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-var http = require("http");
+var fs = require("fs");
 
-http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    
-    response.end("Hello World\n");
-}).listen(8081);
+var data = fs.readFileSync('input.txt');
 
-console.log("Server running at http://127.0.0.1:8081");
+console.log(data.toString());
+console.log("Program has ended");
+
+fs.readFile('input.txt', function(err,data){
+    if (err) return console.error(err);
+    console.log(data.toString());
+})
+console.log("non block Program has ended");
