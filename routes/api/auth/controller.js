f0494ee4d196e2a2024b6e9d5508149800fff833
brainbackdoor <brainbackdoor@gmail.com>
2018-02-15 21:41:37 +0900

    Node JS Study - Step 01 JWT/로그인 API 기능 구현

--- routes/api/auth/controller.js
+++ routes/api/auth/controller.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const User = require('../../../models/user')
 
 /*
@@ -56,6 +57,69 @@ exports.register = (req, res) => {
     .then(create)
     .then(count)
     .then(assign)
+    .then(respond)
+    .catch(onError)
+}
+
+/*
+    POST /api/auth/login
+    {
+        username,
+        password
+    }
+*/
+
+exports.login = (req, res) => {
+    const { username, password } = req.body
+    const secret = req.app.get('jwt-secret')
+
+    // check the user info & generate the jwt
+    const check = (user) => {
+        if(!user) {
+            throw new Error('login failed')
+        } else {
+            if (user.verify(password)){
+                const p = new Promise((resolve, reject) => {
+                    jwt.sign({
+                        _id: user._id,
+                        username: user.username,
+                        admin: user.admin
+                    },
+                    SeCrEtKeYfOrHaShInG,
+                    {
+                        expiresIn: '7d',
+                        issuer: 'brainbackdoor.com',
+                        subject: 'userInfo'
+                    }, (err, token) => {
+                        if(err) reject (err)
+                        resolve(token)
+                    })
+                })
+                return p
+            } else {
+                throw new Error ('login failed')
+            }
+        }
+    }
+
+    // respond the token
+    const respond = (token) => {
+        res.json({
+            message: 'logged in successfully',
+            token
+        })
+    }
+
+    // error occured
+    const onError = (error) => {
+        res.status(403).json({
+            message: error.message
+        })
+    }
+
+    // find the user
+    User.findOneByUsername(username)
+    .then(check)
     .then(respond)
     .catch(onError)
 }
