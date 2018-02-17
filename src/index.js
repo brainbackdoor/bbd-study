75664118765a5b0f2a3b74eacf5491be30e9f6c1
brainbackdoor <brainbackdoor@gmail.com>
2018-02-17 22:05:19 +0900

    Node JS Study - Step 02 - redux 초기 설정

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// Router
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// Container Components
 import { App, Home, Login, Register } from 'containers';
+
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, applyMiddleware(thunk))
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-    <BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
         <div>
-        <Route exact path="/" component={App}/>    
-        <Route exact path="/" component={Home}/>
-        
-        <Route exact path="/login" component={Login}/>
-        <Route exact path="/register" component={Register}/>
-
-            
+            <Route exact path="/" component={App}/>    
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/register" component={Register}/>
         </div>
-    </BrowserRouter>, rootElement
+        </BrowserRouter>
+    </Provider>, rootElement
 );
