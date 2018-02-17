a1c94e163318b291a447f1ecfcd8fa0c53913a4d
brainbackdoor <brainbackdoor@gmail.com>
2018-02-17 13:26:52 +0900

    Node JS Study - Step 02 - Model/account bcryptjs 적용

--- server/models/account.js
+++ server/models/account.js
@@ -8,4 +8,14 @@ const Account = new Schema({
     created: { type: Date, default: Date.now }
 });
 
+// generates hash 
+Account.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, 8);
+};
+
+// compares the password
+Account.methods.validateHash = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
 export default mongoose.model('account', Account);
