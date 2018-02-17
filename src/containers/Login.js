7e4ea80a45be1825b886e017b8efafe0cf605e5c
brainbackdoor <brainbackdoor@gmail.com>
2018-02-18 00:11:04 +0900

    Node JS Study - Step 02 - Login container component를 redux에 연결

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Authentication } from 'components';
+import { connect } from 'react-redux';
+import { loginRequest } from 'actions/authentication';
 
 class Login extends React.Component {
     render() {
@@ -11,4 +13,18 @@ class Login extends React.Component {
     }
 }
 
-export default Login;
+const mapStateToProps = (state) => {
+    return {
+        status: state.authentication.login.status
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        loginRequest: (id, pw) => {
+            return dispatch(loginRequest(id,pw));
+        }
+    };
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(Login);
