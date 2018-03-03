f3772ab9a23bc9d9f59d4c96daa225dca181c68f
brainbackdoor <brainbackdoor@gmail.com>
2018-03-03 17:08:05 +0900

    Django Study - Step 00 - 웹 애플리케이션 확장하기

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from .models import Post
 
@@ -8,7 +8,13 @@ def post_list(request):
     qs = Post.objects.all()
     qs = qs.filter(published_date__lte = timezone.now())
     qs = qs.order_by('published_date')
-    
+
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
+    })
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'blog/post_detail.html', {
+        'post': post
     })
