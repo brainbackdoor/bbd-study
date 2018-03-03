1e8334a721710f11d89b8aa9999f621ae75d499d
brainbackdoor <brainbackdoor@gmail.com>
2018-03-03 17:34:51 +0900

    Django Study - Step 00 - 장고 폼 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+
 
 # Create your views here.
 def post_list(request):
@@ -18,3 +20,39 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {
         'post': post
     })
+    
+    
+def post_new(request):
+
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {
+        'form': form,
+    })
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {
+        'form': form,
+        
+    })        
