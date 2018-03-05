13506d6c8dd6e39385142238f87addbbaea4241e
brainbackdoor <brainbackdoor@gmail.com>
2018-03-06 02:53:28 +0900

    [Controller] 테스트를 위한 불필요한 데이터 삭제

--- motherchoice/src/main/java/com/educhoice/motherchoice/MotherchoiceApplication.java
+++ motherchoice/src/main/java/com/educhoice/motherchoice/MotherchoiceApplication.java
@@ -1,24 +1,9 @@
 package com.educhoice.motherchoice;
 
-import com.educhoice.motherchoice.configuration.security.service.social.SocialSigninProviders;
-import com.educhoice.motherchoice.models.persistent.Academy;
-import com.educhoice.motherchoice.models.persistent.Course;
-import com.educhoice.motherchoice.models.persistent.authorization.Account;
-import com.educhoice.motherchoice.models.persistent.authorization.BasicAccount;
-import com.educhoice.motherchoice.models.persistent.authorization.CorporateAccount;
-import com.educhoice.motherchoice.models.persistent.repositories.AcademyRepository;
-import com.educhoice.motherchoice.models.persistent.repositories.AccountRepository;
-import com.educhoice.motherchoice.models.persistent.repositories.CorporateAccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.educhoice.motherchoice.models.persistent.repositories")
@@ -28,25 +13,4 @@ public class MotherchoiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MotherchoiceApplication.class, args);
 	}
-
-	@Bean
-    public CommandLineRunner commandLineRunner(CorporateAccountRepository repository, AccountRepository accountRepository, PasswordEncoder passwordEncoder) {
-	    return args -> {
-            CorporateAccount account = new CorporateAccount("wheejuni", "1234", BasicAccount.AccountRoles.UNPAID_USER);
-            account.encryptPassword(passwordEncoder);
-
-
-            Account parentAccount = new Account("정휘준", "1234", "봄이네집", null, null, true, BasicAccount.AccountRoles.UNPAID_USER, SocialSigninProviders.KAKAO, 705682990L);
-
-            accountRepository.save(parentAccount);
-            repository.save(account);
-        };
-    }
-
-    @Bean
-    public CommandLineRunner academyInfoBootstrapper(AcademyRepository repository) {
-	    //TODO write dummy academy info bootstrapping logic.
-	    return null;
-    }
-
 }
