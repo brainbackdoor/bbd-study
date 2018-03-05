f97d359462bc8bc49ef48f8f97faafd0ce963605
brainbackdoor <brainbackdoor@gmail.com>
2018-03-06 02:36:11 +0900

    [Controller] 버전1 기능과 무관한 redis 설정 및 기능 주석

--- motherchoice/src/main/java/com/educhoice/motherchoice/configuration/cache/CacheConfig.java
+++ motherchoice/src/main/java/com/educhoice/motherchoice/configuration/cache/CacheConfig.java
@@ -1,38 +1,38 @@
-package com.educhoice.motherchoice.configuration.cache;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.hash.Jackson2HashMapper;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-@Configuration
-@EnableCaching
-public class CacheConfig extends CachingConfigurerSupport{
-
-    @Value("${redis.server.hostname}")
-    private String redisHostname;
-
-    @Value("${redis.server.port}")
-    private int port;
-
-    @Bean
-    public JedisConnectionFactory redisConnectionFactory() {
-
-        JedisConnectionFactory factory = new JedisConnectionFactory();
-
-        factory.setHostName(redisHostname);
-        factory.setPort(port);
-        return factory;
-    }
-
-    @Bean
-    public Jackson2HashMapper jackson2HashMapper() {
-        return new Jackson2HashMapper(new ObjectMapper(), true);
-    }
-}
+//package com.educhoice.motherchoice.configuration.cache;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cache.annotation.CachingConfigurerSupport;
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.hash.Jackson2HashMapper;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//@Configuration
+//@EnableCaching
+//public class CacheConfig extends CachingConfigurerSupport{
+//
+//    @Value("${redis.server.hostname}")
+//    private String redisHostname;
+//
+//    @Value("${redis.server.port}")
+//    private int port;
+//
+//    @Bean
+//    public JedisConnectionFactory redisConnectionFactory() {
+//
+//        JedisConnectionFactory factory = new JedisConnectionFactory();
+//
+//        factory.setHostName(redisHostname);
+//        factory.setPort(port);
+//        return factory;
+//    }
+//
+//    @Bean
+//    public Jackson2HashMapper jackson2HashMapper() {
+//        return new Jackson2HashMapper(new ObjectMapper(), true);
+//    }
+//}
