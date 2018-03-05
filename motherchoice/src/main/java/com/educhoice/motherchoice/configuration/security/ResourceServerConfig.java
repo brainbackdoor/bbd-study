2a8e9a2720b888f3ab0db60d1830cc4ec44b8fdb
brainbackdoor <brainbackdoor@gmail.com>
2018-03-06 02:51:18 +0900

    [Controller] WebSecurityConfigurerAdapter가 HttpSecurity를 우선 Override 함에 따라 설정 변경 & 테스트를 위해 /api url에 대한 authencated 해제

--- motherchoice/src/main/java/com/educhoice/motherchoice/configuration/security/ResourceServerConfig.java
+++ motherchoice/src/main/java/com/educhoice/motherchoice/configuration/security/ResourceServerConfig.java
@@ -1,88 +1,26 @@
 package com.educhoice.motherchoice.configuration.security;
 
-import com.educhoice.motherchoice.configuration.security.service.filters.FormLoginJwtAutheticationFilter;
-import com.educhoice.motherchoice.configuration.security.service.filters.JwtAuthenticationFilter;
-import com.educhoice.motherchoice.configuration.security.service.social.IntegratedLoginAuthenticationManager;
-import com.educhoice.motherchoice.service.JwtIdService;
-import com.educhoice.motherchoice.utils.exceptions.resolvers.security.RestSecurityEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    @Autowired
-    private ResourceServerTokenServices tokenService;
+	@Autowired
+	private ResourceServerTokenServices tokenService;
 
-    @Autowired
-    private IntegratedLoginAuthenticationManager integratedLoginAuthenticationManager;
+	@Value("testjwtresourceid")
+	private String resourceIds;
 
-    @Autowired
-    private RestSecurityEntryPoint entryPoint;
-
-    @Qualifier("accessTokenConverter")
-    @Autowired
-    private JwtAccessTokenConverter tokenConverter;
-
-    @Autowired
-    private JwtIdService jwtIdService;
-
-    @Value("testjwtresourceid")
-    private String resourceIds;
-
-    @Value("realm")
-    private String securityRealm;
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId(resourceIds).tokenServices(tokenService);
-    }
-
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
-
-
-
-        httpSecurity
-                .exceptionHandling()
-                .authenticationEntryPoint(entryPoint);
-
-        httpSecurity
-                .requestMatchers()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/api/v1/**").authenticated()
-                .and()
-                .addFilterAfter(new FormLoginJwtAutheticationFilter("/formlogin", integratedLoginAuthenticationManager, tokenConverter, jwtIdService), CorsFilter.class)
-                .addFilterAfter(new JwtAuthenticationFilter("/tokenauth", integratedLoginAuthenticationManager, tokenConverter, jwtIdService), FormLoginJwtAutheticationFilter.class);
-
-        httpSecurity
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .httpBasic()
-                .realmName(securityRealm);
-
-        httpSecurity
-                .headers()
-                .frameOptions()
-                .disable()
-                .and()
-                .csrf()
-                .disable();
-    }
-
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.resourceId(resourceIds).tokenServices(tokenService);
+	}
 
 }
