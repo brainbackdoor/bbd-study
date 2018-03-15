95a2582df35c5e2db34053e94f62648e39866fd2
brainbackdoor <brainbackdoor@gmail.com>
2018-03-15 23:39:05 +0900

    django-extenstions 앱 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','title','created_at','updated_at']
+
+# admin.site.register(Post, PostAdmin)
 
 
 # Register your models here.
