e9bd8a7030fe4d674c64be7f392aba2ba9ae898d
brainbackdoor <brainbackdoor@gmail.com>
2018-05-13 16:33:41 +0900

    [JAVA][토비의스프링] 1.2.1 관심사의 분리

--- java/toby/week01/example/src/main/java/com/study/brainbackdoor/user/dao/UserDao.java
+++ java/toby/week01/example/src/main/java/com/study/brainbackdoor/user/dao/UserDao.java
@@ -14,12 +14,30 @@ import java.sql.*;
         - 추가적으로, JDBC API Exception 처리가 필요함
  */
 
-public class UserDao {
+/*
+    리팩토링 : 기존의 코드를 외부의 동작방식에는 변화없이 내부 구조를 변경해서 재구성하는 작업을 말한다.
+ */
+
+/*
+    슈퍼클래스에 기본적인 로직의 흐름(커넥션 가져오기, SQL 생성, 실행, 반환)을 만들고,
+    그기능의 일부를 추상 메소드나 오버라이딩이 가능한 protected 메소드 등으로 만든 뒤 서브 클래스에서 이런 메소드를 필요에 맞게 구현해서 사용하도록 하는 방법을
+    디자인 패턴에서 '템플릿 메소드 패턴' 이라고 한다.
+
+    그리고 UserDao의 서브클래스의 getConnection() 메소드는 어떤 Connection 클래스의 오브젝트를 어덯게 생성할 지를 결정하는 방법이라고도 볼 수 있다.
+    이렇게 서브클래스에서 구체적인 오브젝트 생성 방법을 결정하게 하는 것을 팩토리 메소드 패턴이라고 부르기도 한다.
+ */
+
+/*
+    상속은 한계점이 있다.
+    1. 자바는 다중상속을 허용하지 않는다.
+    2. 상속관계는 생각보다 밀접하다.
+    3. 다른 클래스들이 계속 만들어질 경우 getConnection() 구현코드가 중복된다.
+ */
+
+public abstract class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost/studydb", "bbd", "bbd");
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
@@ -30,9 +48,10 @@ public class UserDao {
         c.close();
     }
 
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/studydb", "bbd", "bbd");
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
 
