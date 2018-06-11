a5b4f9f81366e4afb5f290b9276ae76492f33da8
LeeDongGyu <ldg1130@echomarketing.co.kr>
2018-06-11 19:06:07 +0900

    [JAVA] Spring Redis Step01

---
+++ java/spring-redis/src/main/java/com/example/demo/User.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    String name;
+    String sex;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) &&
+                Objects.equals(sex, user.sex);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, sex);
+    }
+}
