c6b2e5f08473329a09aa3e53cfcb6fb616cea1d5
brainbackdoor <brainbackdoor@gmail.com>
2018-12-18 15:36:06 +0900

    Add Primary annotation

--- java/playground-spring2/src/main/java/com/brainbackdoor/playgroundspring2/MyBookRepository.java
+++ java/playground-spring2/src/main/java/com/brainbackdoor/playgroundspring2/MyBookRepository.java
@@ -1,7 +1,8 @@
 package com.brainbackdoor.playgroundspring2;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository @Primary
 public class MyBookRepository implements BookRepository{
 }
