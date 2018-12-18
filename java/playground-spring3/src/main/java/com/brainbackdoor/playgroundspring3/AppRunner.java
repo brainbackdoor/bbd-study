141952c9d4838dfe44030fd71db324a6f4729411
brainbackdoor <brainbackdoor@gmail.com>
2018-12-18 17:16:27 +0900

    Add Study scope prototype

--- java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/AppRunner.java
+++ java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/AppRunner.java
@@ -3,19 +3,30 @@ package com.brainbackdoor.playgroundspring3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
     @Autowired
-    Single single;
-
-    @Autowired
-    Proto proto;
+    ApplicationContext ctx;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(proto);
-        System.out.println(single.getProto());
+        System.out.println("proto");
+        System.out.println(ctx.getBean(Proto.class));
+        System.out.println(ctx.getBean(Proto.class));
+        System.out.println(ctx.getBean(Proto.class));
+
+        System.out.println("single");
+        System.out.println(ctx.getBean(Single.class));
+        System.out.println(ctx.getBean(Single.class));
+        System.out.println(ctx.getBean(Single.class));
+
+        // 계속 같음.. 원래 바껴야 할텐데..
+        System.out.println("proto by single");
+        System.out.println(ctx.getBean(Single.class).getProto());
+        System.out.println(ctx.getBean(Single.class).getProto());
+        System.out.println(ctx.getBean(Single.class).getProto());
     }
 }
--- java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/Proto.java
+++ java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/Proto.java
@@ -1,7 +1,8 @@
 package com.brainbackdoor.playgroundspring3;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component @Scope("prototype")
 public class Proto {
 }
