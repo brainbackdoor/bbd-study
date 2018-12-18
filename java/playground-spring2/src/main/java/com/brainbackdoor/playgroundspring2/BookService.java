2cc6004058365fc26df10a9b8bb68ad2451c2d32
brainbackdoor <brainbackdoor@gmail.com>
2018-12-18 15:40:32 +0900

    Add ApplicationRunner & Qualifier

--- java/playground-spring2/src/main/java/com/brainbackdoor/playgroundspring2/BookService.java
+++ java/playground-spring2/src/main/java/com/brainbackdoor/playgroundspring2/BookService.java
@@ -1,10 +1,15 @@
 package com.brainbackdoor.playgroundspring2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
-    @Autowired
+    @Autowired @Qualifier("bbdRepository")
     BookRepository bookRepository;
+
+    public void printBookRepository() {
+        System.out.println(bookRepository.getClass());
+    }
 }
