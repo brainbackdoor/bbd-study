ff124e45791213fab043a0e23e2ba5b116ec8f4e
brainbackdoor <brainbackdoor@gmail.com>
2018-12-18 20:52:56 +0900

    Convert environment to Profile

--- java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/TestBookRepository.java
+++ java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/TestBookRepository.java
@@ -1,4 +1,13 @@
 package com.brainbackdoor.playgroundspring3;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+/*
+    profile 표현식 : !(not), &(and), |(or)
+ */
+@Repository
+@Profile("!prod")
 public class TestBookRepository implements BookRepository {
 }
