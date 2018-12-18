d861fd2047e6b10919edb19d50af4273efe2122b
brainbackdoor <brainbackdoor@gmail.com>
2018-12-18 22:46:47 +0900

    Add resource

---
+++ java/playground-spring6/src/main/java/com/brainbackdoor/playgroundspring6/PlaygroundSpring6Application.java
@@ -0,0 +1,14 @@
+package com.brainbackdoor.playgroundspring6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundSpring6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundSpring6Application.class, args);
+    }
+
+}
+
---
+++ java/playground-spring6/src/test/java/com/brainbackdoor/playgroundspring6/PlaygroundSpring6ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.brainbackdoor.playgroundspring6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlaygroundSpring6ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
