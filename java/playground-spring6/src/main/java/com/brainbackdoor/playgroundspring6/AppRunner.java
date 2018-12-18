326e65166d551d4aa7cec5f3847a782e30a3c909
brainbackdoor <brainbackdoor@gmail.com>
2018-12-19 06:32:09 +0900

    Add resource description

--- java/playground-spring6/src/main/java/com/brainbackdoor/playgroundspring6/AppRunner.java
+++ java/playground-spring6/src/main/java/com/brainbackdoor/playgroundspring6/AppRunner.java
@@ -3,6 +3,8 @@ package com.brainbackdoor.playgroundspring6;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -19,9 +21,13 @@ public class AppRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        FileSystemXmlApplicationContext ctx =
+                //app.xml => resource
+//                new ClassPathXmlApplicationContext("app,xml");
+                //Filesystem 경로를 기준으로 app.xml을 찾게됨
+                new FileSystemXmlApplicationContext("app,xml");
         Resource resource = resourceLoader.getResource("classpath:test.txt");
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
-        System.out.println(Files.readString(Path.of(resource.getURI())));
     }
 }
