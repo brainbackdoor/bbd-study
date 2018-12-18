169ead8e000f2f40037642a998330d094ba67846
brainbackdoor <brainbackdoor@gmail.com>
2018-12-18 17:13:28 +0900

    Add Study scope singleton

---
+++ java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/PlaygroundSpring3Application.java
@@ -0,0 +1,14 @@
+package com.brainbackdoor.playgroundspring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundSpring3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundSpring3Application.class, args);
+    }
+
+}
+
---
+++ java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/Proto.java
@@ -0,0 +1,7 @@
+package com.brainbackdoor.playgroundspring3;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Proto {
+}
