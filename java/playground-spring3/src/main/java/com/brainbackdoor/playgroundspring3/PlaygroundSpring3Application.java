b45726e873a23685f371279641e89d1a9067f2a4
brainbackdoor <brainbackdoor@gmail.com>
2018-12-18 21:10:11 +0900

    Compare vm options vs properties

--- java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/PlaygroundSpring3Application.java
+++ java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/PlaygroundSpring3Application.java
@@ -2,8 +2,10 @@ package com.brainbackdoor.playgroundspring3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/app.properties")
 public class PlaygroundSpring3Application {
 
     public static void main(String[] args) {
