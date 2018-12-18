df0bc5d68419e47d66c4f981932f476f3661ee33
brainbackdoor <brainbackdoor@gmail.com>
2018-12-18 17:32:50 +0900

    Add comment of singleton

--- java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/Single.java
+++ java/playground-spring3/src/main/java/com/brainbackdoor/playgroundspring3/Single.java
@@ -9,6 +9,10 @@ public class Single {
     @Autowired
     Proto proto; // proxy bean이 주입
 
+    int value = 0;
+    // singleton에서는 Property가 공유되므로 Thread-safe하지 않다.
+    // 그리고 Singleton은 ApplicationContext 초기 구동시 인스턴스를 생성한다.
+
     public Proto getProto() {
         return proto;
     }
