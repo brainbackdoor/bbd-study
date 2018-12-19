f9308349bfb6e25697b0720796b862f8ee1f8f80
brainbackdoor <brainbackdoor@gmail.com>
2018-12-19 11:13:54 +0900

    Add converter

---
+++ java/playground-spring7/src/main/java/com/brainbackdoor/playgroundspring7/EventConverter.java
@@ -0,0 +1,23 @@
+package com.brainbackdoor.playgroundspring7;
+
+import org.springframework.core.convert.converter.Converter;
+
+public class EventConverter {
+    /*
+        상태정보가 없기 때문에 빈에 등록해도 상관없다.
+     */
+    public static class StringToEventConverter implements Converter<String, Event> {
+
+        @Override
+        public Event convert(String source) {
+            return new Event(Integer.parseInt(source));
+        }
+    }
+    public static class EventToStringConverter implements Converter<Event, String> {
+
+        @Override
+        public String convert(Event source) {
+            return source.getId().toString();
+        }
+    }
+}
---
+++ java/playground-spring7/src/main/java/com/brainbackdoor/playgroundspring7/WebConfig.java
@@ -0,0 +1,13 @@
+package com.brainbackdoor.playgroundspring7;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new EventConverter.StringToEventConverter());
+    }
+}
