86897de6fdb05e4e7a286dfb1755fb16d96f4846
brainbackdoor <brainbackdoor@gmail.com>
2018-12-19 11:36:04 +0900

    Check all converter

--- java/playground-spring7/src/main/java/com/brainbackdoor/playgroundspring7/AppRunner.java
+++ java/playground-spring7/src/main/java/com/brainbackdoor/playgroundspring7/AppRunner.java
@@ -13,6 +13,8 @@ public class AppRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        // 모든 converter 확인하기
+        System.out.println(conversionService);
         System.out.println(conversionService.getClass().toGenericString());
         // WebConversionService은 DefaultFormattingConversionService를 상속받아 확장하여 구현
 //        conversionService.convert()
