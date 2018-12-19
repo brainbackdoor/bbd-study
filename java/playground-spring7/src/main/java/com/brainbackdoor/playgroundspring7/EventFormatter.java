213326cc8d41256da7f80ab459fd9e8967454ac5
brainbackdoor <brainbackdoor@gmail.com>
2018-12-19 11:19:29 +0900

    Add formmater

---
+++ java/playground-spring7/src/main/java/com/brainbackdoor/playgroundspring7/EventFormatter.java
@@ -0,0 +1,23 @@
+package com.brainbackdoor.playgroundspring7;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+// Formatter도 빈으로 등록해서 써도 된다.
+public class EventFormatter implements Formatter<Event> {
+
+    @Override
+    public Event parse(String text, Locale locale) throws ParseException {
+        return new Event(Integer.parseInt(text));
+    }
+
+    @Override
+    public String print(Event object, Locale locale) {
+        return object.getId().toString();
+    }
+}
