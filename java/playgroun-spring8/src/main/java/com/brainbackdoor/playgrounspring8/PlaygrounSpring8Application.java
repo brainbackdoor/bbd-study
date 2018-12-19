58a8729629ff523e8af2dc75b0ce2b1233a46242
brainbackdoor <brainbackdoor@gmail.com>
2018-12-19 17:05:30 +0900

    Add spring Expression Language

---
+++ java/playgroun-spring8/src/main/java/com/brainbackdoor/playgrounspring8/PlaygrounSpring8Application.java
@@ -0,0 +1,14 @@
+package com.brainbackdoor.playgrounspring8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygrounSpring8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygrounSpring8Application.class, args);
+    }
+
+}
+
