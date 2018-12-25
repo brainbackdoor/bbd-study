ad6e0f32c15f71252a92ad72e26b0a9cba6039ea
brainbackdoor <brainbackdoor@gmail.com>
2018-12-25 23:53:04 +0900

    Add study spring data & db migration by flyway

---
+++ java/springdatajpa/src/main/java/com/brainbackdoor/springdatajpa/SpringdatajpaApplication.java
@@ -0,0 +1,16 @@
+package com.brainbackdoor.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+
+
+@SpringBootApplication
+public class SpringdatajpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdatajpaApplication.class, args);
+    }
+
+}
+
