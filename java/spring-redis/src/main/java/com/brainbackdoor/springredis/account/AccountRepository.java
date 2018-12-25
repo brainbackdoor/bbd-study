7e8dc481bdde24a9dd4bc768ed5932808098f795
brainbackdoor <brainbackdoor@gmail.com>
2018-12-26 08:40:06 +0900

    Add study spring redis basic

---
+++ java/spring-redis/src/main/java/com/brainbackdoor/springredis/account/AccountRepository.java
@@ -0,0 +1,6 @@
+package com.brainbackdoor.springredis.account;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<Account, String> {
+}
