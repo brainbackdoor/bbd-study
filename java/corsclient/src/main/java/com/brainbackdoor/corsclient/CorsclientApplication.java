9783d3ee623f699607b7fe4f1593d5a95dda8d39
brainbackdoor <brainbackdoor@gmail.com>
2018-12-25 16:26:19 +0900

    Add study spring cors

---
+++ java/corsclient/src/main/java/com/brainbackdoor/corsclient/CorsclientApplication.java
@@ -0,0 +1,14 @@
+package com.brainbackdoor.corsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorsclientApplication.class, args);
+    }
+
+}
+
