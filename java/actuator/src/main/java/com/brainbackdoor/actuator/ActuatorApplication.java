5028f3831bf84ba4767e1e3ccec978976994e2b8
brainbackdoor <brainbackdoor@gmail.com>
2018-12-27 06:03:32 +0900

    Add study spring operation by actuator,admin

---
+++ java/actuator/src/main/java/com/brainbackdoor/actuator/ActuatorApplication.java
@@ -0,0 +1,22 @@
+package com.brainbackdoor.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ActuatorApplication {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+}
+
