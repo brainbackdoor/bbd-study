b31726ef09a4ced90c8d728156792b4cd0ed873c
brainbackdoor <brainbackdoor@gmail.com>
2019-01-02 23:04:58 +0900

    Add JPA study setting

---
+++ java/springboot/playground-jpa01/src/main/java/com/brainbackdoor/playgroundjpa01/PlaygroundJpa01Application.java
@@ -0,0 +1,18 @@
+package com.brainbackdoor.playgroundjpa01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class PlaygroundJpa01Application {
+
+    public static void main(String[] args) throws SQLException {
+        SpringApplication.run(PlaygroundJpa01Application.class, args);
+    }
+
+}
+
