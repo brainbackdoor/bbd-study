c74ceaec99a8fbf787c88a2ebbb2c8151a6a96a1
brainbackdoor <brainbackdoor@gmail.com>
2019-01-07 10:51:17 +0900

    Add study spring-jpa create query

--- java/springboot/playground-jpa01/src/main/java/com/brainbackdoor/playgroundjpa01/CommentRepository.java
+++ java/springboot/playground-jpa01/src/main/java/com/brainbackdoor/playgroundjpa01/CommentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 //@RepositoryDefinition(domainClass = Comment.class, idClass = Long.class)
 //public interface CommentRepository {
@@ -17,8 +18,17 @@ public interface CommentRepository extends MyRepository<Comment, Long> {
 
 //    @Query(value = "SELECT c FROM Comment As c", nativeQuery = true) // SQL
 //    @Query(value = "SELECT c FROM Comment As c") // JPQL
-    List<Comment> findByCommentContains(String keyword); // 이름을 보고 유추
+//    List<Comment> findByCommentContains(String keyword); // 이름을 보고 유추
+//
+//    Page<Comment> findByLikeCountGreaterThanAndPost(int likeCount, Post post, Pageable pageable);
 
-    Page<Comment> findByLikeCountGreaterThanAndPost(int likeCount, Post post, Pageable pageable);
+
+    // 대문자로 바꿔서 비교함
+//    List<Comment> findByCommentContainsIgnoreCase(String keyword);
+
+    List<Comment> findByCommentContainsIgnoreCaseAndLikeCountGreaterThan(String keyword, Integer likeCount);
+
+//    Page<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
+    Stream<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
 
 }
--- java/springboot/playground-jpa01/src/test/java/com/brainbackdoor/playgroundjpa01/CommentRepositoryTest.java
+++ java/springboot/playground-jpa01/src/test/java/com/brainbackdoor/playgroundjpa01/CommentRepositoryTest.java
@@ -4,10 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,6 +44,33 @@ public class CommentRepositoryTest {
 
     @Test
     public void crud2() {
+        createComment(100, "spring data jpa");
+
+        List<Comment> comments = repository.findByCommentContainsIgnoreCaseAndLikeCountGreaterThan("Spring", 10);
+        assertThat(comments.size()).isEqualTo(1);
 
     }
+
+    private void createComment(int likeCount, String comment) {
+        Comment newComment = new Comment();
+        newComment.setComment(comment);
+        newComment.setLikeCount(likeCount);
+        repository.save(newComment);
+    }
+
+    @Test
+    public void crud3() {
+        createComment(100, "spring data jpa");
+        createComment(55, "HIBERNATE SPRING");
+
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "LikeCount"));
+//        Page<Comment> comments = repository.findByCommentContainsIgnoreCase("Spring", pageRequest);
+//        assertThat(comments.getNumberOfElements()).isEqualTo(2);
+//        assertThat(comments).first().hasFieldOrPropertyWithValue("likeCount", 100);
+        try(Stream<Comment> comments = repository.findByCommentContainsIgnoreCase("Spring", pageRequest)) {
+            Comment firstComment = comments.findFirst().get();
+            assertThat(firstComment.getLikeCount()).isEqualTo(100);
+        }
+    }
+
 }
