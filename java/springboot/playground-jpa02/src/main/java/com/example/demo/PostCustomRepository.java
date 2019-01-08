3fc9323a379691fd128fc6fd85eea9b00b92c2f2
brainbackdoor <brainbackdoor@gmail.com>
2019-01-08 21:03:15 +0900

    Add study spring-jpa custom repository

---
+++ java/springboot/playground-jpa02/src/main/java/com/example/demo/PostCustomRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import java.util.List;
+
+public interface PostCustomRepository<T> {
+    List<Post> findMyPost();
+
+    void delete(T entity);
+}
---
+++ java/springboot/playground-jpa02/src/main/java/com/example/demo/PostCustomRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@Transactional
+public class PostCustomRepositoryImpl implements PostCustomRepository<Post> {
+    @Autowired
+    EntityManager entityManager;
+
+    @Override
+    public List<Post> findMyPost() {
+        System.out.println("custom findMyPost");
+        return entityManager.createQuery("SELECT p FROM Post AS p", Post.class)
+                .getResultList();
+    }
+
+    @Override
+    public void delete(Post entity) {
+        System.out.println("delete custom");
+        entityManager.remove(entity);
+    }
+
+}
