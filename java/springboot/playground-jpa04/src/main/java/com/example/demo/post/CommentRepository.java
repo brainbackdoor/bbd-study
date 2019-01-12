fe61c5adfb370b428fb30d56065af1e49c14f5c2
brainbackdoor <brainbackdoor@gmail.com>
2019-01-13 01:24:48 +0900

    Add study spring-jpa query by example

--- java/springboot/playground-jpa04/src/main/java/com/example/demo/post/CommentRepository.java
+++ java/springboot/playground-jpa04/src/main/java/com/example/demo/post/CommentRepository.java
@@ -3,11 +3,14 @@ package com.example.demo.post;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpecificationExecutor<Comment> {
+public interface CommentRepository extends JpaRepository<Comment, Long>,
+        JpaSpecificationExecutor<Comment>,
+        QueryByExampleExecutor<Comment> {
 
     @EntityGraph(value = "Comment.post")
     Optional<Comment> getById(Long id);
