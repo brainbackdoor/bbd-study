054187cc78a14da5d2e8c30893825e7341f18a7f
brainbackdoor <brainbackdoor@gmail.com>
2019-01-13 12:23:40 +0900

    Add study spring-jpa auditing

--- java/springboot/playground-jpa04/src/main/java/com/example/demo/post/Comment.java
+++ java/springboot/playground-jpa04/src/main/java/com/example/demo/post/Comment.java
@@ -1,10 +1,18 @@
 package com.example.demo.post;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@NamedEntityGraph(name = "Comment.post",
-        attributeNodes = @NamedAttributeNode("post"))
+//@NamedEntityGraph(name = "Comment.post",
+//        attributeNodes = @NamedAttributeNode("post"))
+@EntityListeners(AuditingEntityListener.class)
 public class Comment {
     @Id
     @GeneratedValue
@@ -21,6 +29,20 @@ public class Comment {
     private int down;
 
     private boolean best;
+
+    @CreatedDate
+    private Date created;
+
+    @CreatedBy
+    @ManyToOne
+    private Account createdBy;
+
+    @LastModifiedDate
+    private Date updated;
+
+    @LastModifiedBy
+    @ManyToOne
+    private Account updatedBy;
 
     public Long getId() {
         return id;
@@ -68,5 +90,11 @@ public class Comment {
 
     public void setBest(boolean best) {
         this.best = best;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        System.out.println("Pre Persist is called");
+        
     }
 }
