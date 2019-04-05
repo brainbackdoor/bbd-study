48ec2e11990ccd75e8b81f4398c32a326dcded67
brainbackdoor <brainbackdoor@gmail.com>
2019-04-06 02:27:38 +0900

    feat(tdd): for study when first week

---
+++ java/project/tdd/src/main/java/com/brainbackdoor/tdd/Money.java
@@ -0,0 +1,10 @@
+package com.brainbackdoor.tdd;
+
+public class Money {
+    protected int amount;
+
+    public boolean equals(Object o) {
+        Money money = (Money) o;
+        return amount == money.amount;
+    }
+}
